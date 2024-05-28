ff22c7d67c286ee9f93cae4fd0b8dd861e9f7324
PHILIP OBIORAH <philip.obiorah@outlook.com>
2024-05-28 23:50:26 +0100

    competed guessing game

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ guess = int(input("Enter any number: "))
 while n!= guess:
     if guess < n:
         print("Too low")
+        guess = int(input("Enter any number: "))
+    elif guess > n:
+        print("Too high")
+        guess = int(input("Enter any number: "))
+    else:
+        break
+    print("You guessed it right !!")
